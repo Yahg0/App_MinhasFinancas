981add34c3d74dbfc37f1914042de157419fe710
Yahgo Batista <Yahgobatista@LIVE.COM>
2018-06-27 19:59:37 -0300

    Adicionando o Serializable a classe Usuario

--- app/src/main/java/up/edu/br/minhasfinancas/Usuario.java
+++ app/src/main/java/up/edu/br/minhasfinancas/Usuario.java
@@ -1,6 +1,8 @@
 package up.edu.br.minhasfinancas;
 
-public class Usuario{
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
 
     private Integer id;
     private String nome;
